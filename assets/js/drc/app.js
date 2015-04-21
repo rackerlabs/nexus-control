50be046479f75088837bf4e2ff242e88c8f4341f
Ash Wilson <smashwilson@gmail.com>
2015-04-21 11:01:57 -0400

    Use an index file to preserve .js order.

--- assets/js/drc/app.js
+++ assets/js/drc/app.js
@@ -1,20 +1,3 @@
-// This is the master, all-in-one JavaScript file that's concatenated from the
-// others under this directory. Rather than adding JavaScript here, add it
-// somewhere else in this directory and it'll be included.
-
-//= require lib/jquery/jquery-1.11.1
-//= require lib/jquery/jquery.cookie-1.4.1
-//= require lib/bootstrap/collapse
-//= require lib/bootstrap/dropdown
-//= require lib/bootstrap/tooltip
-//= require lib/bootstrap/tab
-//= require core.js
-//= require pages/sponsorship.js
-//= require pages/docs.js
-//= require pages/devtrial.js
-//= require pages/signup.js
-//= require pages/home.js
-
 (function (window, document, $) {
   var app = window.devsite;
 
