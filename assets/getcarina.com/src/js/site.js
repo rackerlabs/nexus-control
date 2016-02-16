b285229e0322320ab0b14a364c3b73e6da0494d1
Keith Bartholomew <keith.bartholomew@rackspace.com>
2016-02-16 13:50:58 -0600

    steal the signup form logic and use it on the landing page

--- assets/getcarina.com/src/js/site.js
+++ assets/getcarina.com/src/js/site.js
@@ -9,6 +9,7 @@ angular.module('carina', [
   require('./beta-alert/beta-alert'),
   require('./docs/docs-list'),
   require('./signup/signup'),
+  require('./landing-pages/carina-workshop-form'),
 ]);
 
 angular.bootstrap(document, ['carina']);
