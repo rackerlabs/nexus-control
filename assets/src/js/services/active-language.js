016c9e131ddf76cb9727e842dabe9f01213d43bb
Keith Bartholomew <keith.bartholomew@rackspace.com>
2016-06-21 11:45:26 -0500

    Set the active language if the URL contains ?lang=

--- assets/src/js/services/active-language.js
+++ assets/src/js/services/active-language.js
@@ -8,6 +8,15 @@ var LANGUAGE_CHANGE_EVENT = 'drcLanguageChange';
 
 angular.module(moduleName, [])
 .factory('activeLanguage', ['$rootScope', function ($rootScope) {
+
+    // Bootstrap the active language selection by evaluating a "?lang=" query string
+    var languageSearchPattern = /lang=([^&]+)/;
+    if (window.location.search.match(languageSearchPattern)) {
+      var chosenLanguage = window.location.search.match(languageSearchPattern)[1];
+      localStorage.setItem(LANGUAGE_KEY, chosenLanguage);
+    }
+
+    // If no language has been set, assign a default
     if(! localStorage.getItem(LANGUAGE_KEY)) {
         localStorage.setItem(LANGUAGE_KEY, 'sh');
     }
