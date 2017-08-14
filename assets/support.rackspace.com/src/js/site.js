1cb6f33db7d05de4718c1abeb313d828f7b50498
Alexandra Settle <a.settle@outlook.com>
2017-08-14 10:36:18 +0100

    Add search to support.rax.com

--- assets/support.rackspace.com/src/js/site.js
+++ assets/support.rackspace.com/src/js/site.js
@@ -1,45 +1,17 @@
 var $ = require('jquery');
 window.$ = window.jQuery = $;
 
-var setupHomepage = require('./homepage');
-var setupAllArticles = require('./all-articles');
-var setupAdmonish = require('./admonish');
+var angular = require('angular');
+var markalytics = require('markalytics');
 
-// kick things off
+angular.module('carina', [
+  require('angular-cookies'),
+  require('angular-sanitize'),
+  require('./beta-alert/beta-alert'),
+  require('./docs/docs-list'),
+  require('./signup/signup'),
+  require('./landing-pages/carina-workshop-form'),
+  require('./search')
+]);
 
-$(document).ready(function(){
-
-  // FAQs toggle content
-  $("div.faq h4").nextUntil("h4, h3, hr, .legal-container").hide();
-
-  $("div.faq h4").click(function() {
-    $(this).nextUntil("h4, h3, hr, .legal-container").slideToggle("fast");
-    $(this).toggleClass("active", 1000);
-  });
-
-  // Tags toggle
-  $("#list-tags").click(function() {
-    $(".tags-list").toggle();
-  });
-
-  // setup the homepage stuff
-  if (window.location.pathname.match(/how-to\/$/)) {
-    setupHomepage();
-  }
-
-  if (window.location.pathname.match(/-all-articles\/$/)) {
-    setupAllArticles();
-  }
-
-  setupAdmonish();
-
-  //setup the dropdown on the whitepaper site
-  $('#topics').change(function() {
-    var targetPosition = $($(this).val()).offset().top;
-    $('html,body').animate({
-      scrollTop: targetPosition
-    }, 'slow');
-  });
-
-
-});
+angular.bootstrap(document, ['carina']);
