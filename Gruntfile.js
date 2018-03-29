491c33be05dca9b0baaeeb0ac2f8176fad6c947e
Robb Romans <robb.romans@rackspace.com>
2018-03-29 14:53:21 -0500

    Kill comma

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
                         'fonts/**/*'
                       ],
                       dest: 'assets/dist/'
-                    },
+                    }
                 ]
             }
         },
